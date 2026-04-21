802b9dea6ce5a41c7340aa9080834f3e43801b10
China-Pony <2634236130@qq.com>
2026-04-21 14:01:55 +0800

    原始代码，配好环境，跑通100轮

---
+++ ultralytics/nn/modules/common_utils_mbyolo.py
@@ -0,0 +1,225 @@
+import torch
+import math
+from functools import partial
+from typing import Callable, Any
+
+import torch.nn as nn
+from einops import rearrange, repeat
+from timm.models.layers import DropPath
+
+DropPath.__repr__ = lambda self: f"timm.DropPath({self.drop_prob})"
+try:
+    import selective_scan_cuda_core
+    import selective_scan_cuda_oflex
+    import selective_scan_cuda_ndstate
+    import selective_scan_cuda_nrow
+    import selective_scan_cuda
+except:
+    pass
+
+try:
+    "sscore acts the same as mamba_ssm"
+    import selective_scan_cuda_core
+except Exception as e:
+    print(e, flush=True)
+    "you should install mamba_ssm to use this"
+    SSMODE = "mamba_ssm"
+    import selective_scan_cuda
+    # from mamba_ssm.ops.selective_scan_interface import selective_scan_fn, selective_scan_ref
+
+"""
+对二维特征图做 LayerNorm（先转成 B,H,W,C 再归一化，然后转回），用于稳定训练。
+"""
+class LayerNorm2d(nn.Module):
+
+    def __init__(self, normalized_shape, eps=1e-6, elementwise_affine=True):
+        super().__init__()
+        self.norm = nn.LayerNorm(normalized_shape, eps, elementwise_affine)
+
+    def forward(self, x):
+        x = rearrange(x, 'b c h w -> b h w c').contiguous()
+        x = self.norm(x)
+        x = rearrange(x, 'b h w c -> b c h w').contiguous()
+        return x
+
+"""
+自动计算卷积 padding 使输出尺寸不变。
+"""
+def autopad(k, p=None, d=1):  # kernel, padding, dilation
+    """Pad to 'same' shape outputs."""
+    if d > 1:
+        k = d * (k - 1) + 1 if isinstance(k, int) else [d * (x - 1) + 1 for x in k]  # actual kernel-size
+    if p is None:
+        p = k // 2 if isinstance(k, int) else [x // 2 for x in k]  # auto-pad
+    return p
+
+
+# Cross Scan
+"""
+将一张二维特征图（形状 B,C,H,W）展开成 四个方向 的一维序列
+方向 0：按行展开（从左到右，从上到下）
+
+方向 1：按列展开（从上到下，从左到右）
+
+方向 2 和 3：分别是方向 0 和 1 的翻转（相当于反向扫描）
+"""
+class CrossScan(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, x: torch.Tensor):
+        B, C, H, W = x.shape
+        ctx.shape = (B, C, H, W)
+        xs = x.new_empty((B, 4, C, H * W))
+        xs[:, 0] = x.flatten(2, 3)
+        xs[:, 1] = x.transpose(dim0=2, dim1=3).flatten(2, 3)
+        xs[:, 2:4] = torch.flip(xs[:, 0:2], dims=[-1])
+        return xs
+
+    @staticmethod
+    def backward(ctx, ys: torch.Tensor):
+        # out: (b, k, d, l)
+        B, C, H, W = ctx.shape
+        L = H * W
+        ys = ys[:, 0:2] + ys[:, 2:4].flip(dims=[-1]).view(B, 2, -1, L)
+        y = ys[:, 0] + ys[:, 1].view(B, -1, W, H).transpose(dim0=2, dim1=3).contiguous().view(B, -1, L)
+        return y.view(B, -1, H, W)
+
+
+"""
+将经过 SSM 处理后的四个方向的结果再合并回一张特征图。合并的方式是按对应方向相加（正向+反向），然后按对应位置恢复成二维。
+"""
+class CrossMerge(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, ys: torch.Tensor):
+        B, K, D, H, W = ys.shape
+        ctx.shape = (H, W)
+        ys = ys.view(B, K, D, -1)
+        ys = ys[:, 0:2] + ys[:, 2:4].flip(dims=[-1]).view(B, 2, D, -1)
+        y = ys[:, 0] + ys[:, 1].view(B, -1, W, H).transpose(dim0=2, dim1=3).contiguous().view(B, D, -1)
+        return y
+
+    @staticmethod
+    def backward(ctx, x: torch.Tensor):
+        # B, D, L = x.shape
+        # out: (b, k, d, l)
+        H, W = ctx.shape
+        B, C, L = x.shape
+        xs = x.new_empty((B, 4, C, L))
+        xs[:, 0] = x
+        xs[:, 1] = x.view(B, C, H, W).transpose(dim0=2, dim1=3).flatten(2, 3)
+        xs[:, 2:4] = torch.flip(xs[:, 0:2], dims=[-1])
+        xs = xs.view(B, 4, C, H, W)
+        return xs, None, None
+
+
+# cross selective scan ===============================
+"""
+定义了前向和反向传播，实际计算由 CUDA 完成。
+"""
+class SelectiveScanCore(torch.autograd.Function):
+    # comment all checks if inside cross_selective_scan
+    @staticmethod
+    @torch.cuda.amp.custom_fwd
+    def forward(ctx, u, delta, A, B, C, D=None, delta_bias=None, delta_softplus=False, nrows=1, backnrows=1,
+                oflex=True):
+        # all in float
+        if u.stride(-1) != 1:
+            u = u.contiguous()
+        if delta.stride(-1) != 1:
+            delta = delta.contiguous()
+        if D is not None and D.stride(-1) != 1:
+            D = D.contiguous()
+        if B.stride(-1) != 1:
+            B = B.contiguous()
+        if C.stride(-1) != 1:
+            C = C.contiguous()
+        if B.dim() == 3:
+            B = B.unsqueeze(dim=1)
+            ctx.squeeze_B = True
+        if C.dim() == 3:
+            C = C.unsqueeze(dim=1)
+            ctx.squeeze_C = True
+        ctx.delta_softplus = delta_softplus
+        ctx.backnrows = backnrows
+        out, x, *rest = selective_scan_cuda_core.fwd(u, delta, A, B, C, D, delta_bias, delta_softplus, 1)
+        ctx.save_for_backward(u, delta, A, B, C, D, delta_bias, x)
+        return out
+
+    @staticmethod
+    @torch.cuda.amp.custom_bwd
+    def backward(ctx, dout, *args):
+        u, delta, A, B, C, D, delta_bias, x = ctx.saved_tensors
+        if dout.stride(-1) != 1:
+            dout = dout.contiguous()
+        du, ddelta, dA, dB, dC, dD, ddelta_bias, *rest = selective_scan_cuda_core.bwd(
+            u, delta, A, B, C, D, delta_bias, dout, x, ctx.delta_softplus, 1
+        )
+        return (du, ddelta, dA, dB, dC, dD, ddelta_bias, None, None, None, None)
+
+"""
+一个高级函数，它接受经过 CrossScan 展开的序列，结合投影权重、SSM 参数（A, B, C, D, delta 等），
+调用 SelectiveScanCore 进行计算，最后通过 CrossMerge 恢复为二维特征图。这个过程对应论文中 SS2D 的整体流程。
+"""
+def cross_selective_scan(
+        x: torch.Tensor = None,
+        x_proj_weight: torch.Tensor = None,
+        x_proj_bias: torch.Tensor = None,
+        dt_projs_weight: torch.Tensor = None,
+        dt_projs_bias: torch.Tensor = None,
+        A_logs: torch.Tensor = None,
+        Ds: torch.Tensor = None,
+        out_norm: torch.nn.Module = None,
+        out_norm_shape="v0",
+        nrows=-1,  # for SelectiveScanNRow
+        backnrows=-1,  # for SelectiveScanNRow
+        delta_softplus=True,
+        to_dtype=True,
+        force_fp32=False,  # False if ssoflex
+        ssoflex=True,
+        SelectiveScan=None,
+        scan_mode_type='default'
+):
+    # out_norm: whatever fits (B, L, C); LayerNorm; Sigmoid; Softmax(dim=1);...
+
+    B, D, H, W = x.shape
+    D, N = A_logs.shape
+    K, D, R = dt_projs_weight.shape
+    L = H * W
+
+    def selective_scan(u, delta, A, B, C, D=None, delta_bias=None, delta_softplus=True):
+        return SelectiveScan.apply(u, delta, A, B, C, D, delta_bias, delta_softplus, nrows, backnrows, ssoflex)
+
+    xs = CrossScan.apply(x)
+
+    x_dbl = torch.einsum("b k d l, k c d -> b k c l", xs, x_proj_weight)
+    if x_proj_bias is not None:
+        x_dbl = x_dbl + x_proj_bias.view(1, K, -1, 1)
+    dts, Bs, Cs = torch.split(x_dbl, [R, N, N], dim=2)
+    dts = torch.einsum("b k r l, k d r -> b k d l", dts, dt_projs_weight)
+    xs = xs.view(B, -1, L)
+    dts = dts.contiguous().view(B, -1, L)
+    # HiPPO matrix
+    As = -torch.exp(A_logs.to(torch.float))  # (k * c, d_state)
+    Bs = Bs.contiguous()
+    Cs = Cs.contiguous()
+    Ds = Ds.to(torch.float)  # (K * c)
+    delta_bias = dt_projs_bias.view(-1).to(torch.float)
+
+    if force_fp32:
+        xs = xs.to(torch.float)
+        dts = dts.to(torch.float)
+        Bs = Bs.to(torch.float)
+        Cs = Cs.to(torch.float)
+
+    ys: torch.Tensor = selective_scan(
+        xs, dts, As, Bs, Cs, Ds, delta_bias, delta_softplus
+    ).view(B, K, -1, H, W)
+
+    y: torch.Tensor = CrossMerge.apply(ys)
+
+    if out_norm_shape in ["v1"]:  # (B, C, H, W)
+        y = out_norm(y.view(B, -1, H, W)).permute(0, 2, 3, 1)  # (B, H, W, C)
+    else:  # (B, L, C)
+        y = y.transpose(dim0=1, dim1=2).contiguous()  # (B, L, C)
+        y = out_norm(y).view(B, H, W, -1)
+
+    return (y.to(x.dtype) if to_dtype else y)
